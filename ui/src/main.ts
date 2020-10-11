257dc60bad7eb24ac1e96237e21d2c4b7e851f5b
Thurstag <sashaberthout@hotmail.com>
2020-10-11 14:53:39 +0200

    [CHG] [UI] Remove full bundle loading for vue-material
    Activate CI for pull requests

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,12 +1,11 @@
 import Vue from "vue";
 import "vue-material/dist/theme/default.css";
 import "vue-material/dist/vue-material.min.css";
-import VueMaterial from "vue-material";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
-Vue.use(VueMaterial);
+// Import material components (N.B: Import one by one and not the full bundle!!!)
 
 new Vue({
     render: h => h(App)
